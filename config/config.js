612ce26e95f9a6e5023a047d066f840283689a08
GOLF <golf_cat1@hotmail.com>
2018-11-28 18:11:25 +0700

    change config

--- config/config.js
+++ config/config.js
@@ -1,9 +1,10 @@
 
 const config = {
-  channelAccessToken : "KdPxuxA2kiJcTnFU/y7+dWyWOfMZfZ8tZPziRyRnaCHKi/rcQfDpERPKiaOWWIgbnN+2LyDzEEKQWEjJ9BRRkzGgHakQoDX5/b/BhqxHPEUy6XxISulLURSYlPwqW3lfS6PUtsYp+NZFU4mv8YbO+wdB04t89/1O/w1cDnyilFU=",
+  channelAccessToken : "g85dSlNenRkPbVqslartx8e0JU5YLmwEK0LD4GX4uPXxpSy3+byEcTJ8A8wD5RUvnN+2LyDzEEKQWEjJ9BRRkzGgHakQoDX5/b/BhqxHPEVsGRv2bdt+2yvP3KN2bh8j8B05ZioGV7GfLlLeIEaWHAdB04t89/1O/w1cDnyilFU=",
 
   testGroup: "C6ffd9f64b6a8e781ad445939d38285da",
   milkTeaGroup : "C74729f37296ad4c96bacc1d76324cdd3"
+  
 }
 // Test Group : C6ffd9f64b6a8e781ad445939d38285da
 // Tea Group : C74729f37296ad4c96bacc1d76324cdd3
