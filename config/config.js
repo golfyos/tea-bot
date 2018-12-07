9a0f6e89377f74c3d433b776b9e90581cc01383e
GOLF <golf_cat1@hotmail.com>
2018-12-07 16:13:24 +0700

    change something

--- config/config.js
+++ config/config.js
@@ -1,6 +1,6 @@
 
 const config = {
-  channelAccessToken : "PTiRRlhHAcUdkHmcbWS01dsg0TUpmMiKgzFQ39TNQaDES6O9cFPjO9jhRhOyMOdAnN+2LyDzEEKQWEjJ9BRRkzGgHakQoDX5/b/BhqxHPEUMQ6cQlSHOXtCLP2/uya9m5BmpaSBMoS4bMvVfM3xk7wdB04t89/1O/w1cDnyilFU=",
+  channelAccessToken : "u+6jGF599gGkuoVzgADgpKHQf9QhoWrcHFkanknBXUAJcqLu/H7Cd0zKNhVH+/dAnN+2LyDzEEKQWEjJ9BRRkzGgHakQoDX5/b/BhqxHPEWrY5n6fFks/bLnWXEeKy8ac1+5bqRqhWXVZxdNr/rzLAdB04t89/1O/w1cDnyilFU=",
 
   testGroup: "C6ffd9f64b6a8e781ad445939d38285da",
   milkTeaGroup : "C74729f37296ad4c96bacc1d76324cdd3"
