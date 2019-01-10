21f4f69ebe15e1685c9e2f434fb76f3080f31c39
GOLF <golf_cat1@hotmail.com>
2019-01-10 13:33:47 +0700

    add admin

--- config/config.js
+++ config/config.js
@@ -1,6 +1,6 @@
 
 const config = {
-  channelAccessToken : "u+6jGF599gGkuoVzgADgpKHQf9QhoWrcHFkanknBXUAJcqLu/H7Cd0zKNhVH+/dAnN+2LyDzEEKQWEjJ9BRRkzGgHakQoDX5/b/BhqxHPEWrY5n6fFks/bLnWXEeKy8ac1+5bqRqhWXVZxdNr/rzLAdB04t89/1O/w1cDnyilFU=",
+  channelAccessToken : "A1ePHVGHjwkdzLmUFfypLiVX4og7c8k5MO2s9qu+SBZwgEM6Y3or9NGSXmHvAS45nN+2LyDzEEKQWEjJ9BRRkzGgHakQoDX5/b/BhqxHPEWlX5gOPCTCybOAHoUtDstqEt9DE4uLa9HhxB4Dhj8xQAdB04t89/1O/w1cDnyilFU=",
 
   testGroup: "C6ffd9f64b6a8e781ad445939d38285da",
   milkTeaGroup : "C74729f37296ad4c96bacc1d76324cdd3"
