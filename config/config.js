38b845306392ca1737a822f718a6818e758f5441
GOLF <golf_cat1@hotmail.com>
2019-01-30 16:24:08 +0700

    ..

--- config/config.js
+++ config/config.js
@@ -1,6 +1,6 @@
 // Test Group : C6ffd9f64b6a8e781ad445939d38285da
 // Tea Group : C74729f37296ad4c96bacc1d76324cdd3
 
-export const channelAccessToken = "vhW9x4PPMUgioH95O2of++9NE9rmzllWMnMglb6rVk/NpcM7HynBWqsDELmkRREwnN+2LyDzEEKQWEjJ9BRRkzGgHakQoDX5/b/BhqxHPEWyF/6/liigoACbqZU6kpuBAoscItQJSVNBRoDKiImHygdB04t89/1O/w1cDnyilFU="
+export const channelAccessToken = "NuYVjz5S9GgDbwx0jMDyl07jD2CXa4lx0ThlTETITNAeTkppmgkswuNc3tbKetxvnN+2LyDzEEKQWEjJ9BRRkzGgHakQoDX5/b/BhqxHPEXnyz9I7VDd9aUQmmVOTiwZ1sjJC/+hoWDgFv/qr7bNKAdB04t89/1O/w1cDnyilFU="
 export const testGroup = "C6ffd9f64b6a8e781ad445939d38285da"
 export const milkTeaGroup = "C74729f37296ad4c96bacc1d76324cdd3"
