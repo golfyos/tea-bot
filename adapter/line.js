c25406c1e41eea00ca5d662b0b1adb89c621917b
golfyos <golf_cat1@hotmail.com>
2020-10-25 12:19:19 +0100

    Updated line adapter: send push message

--- adapter/line.js
+++ adapter/line.js
@@ -1,18 +1,43 @@
-
 import axios from 'axios'
 import querystring from 'querystring'
 
+import config from '../config/config.json'
+import { makeTextMessageObj } from '../utils/data_util'
+
 export class Line {
-    static sendMessage() {
-        
+    static async sendMessage(groupId, message) {
+        const token = await Line.getAccessToken()
+  
+        const data = {
+          to : groupId,
+          messages : [makeTextMessageObj(message)]
+        }
+      
+        const header = {
+            "Content-Type" : "application/json",
+            "Authorization" : "Bearer " + token
+        }
+
+        try {
+            await axios({
+              url: 'https://api.line.me/v2/bot/message/push',
+              method: "POST",
+              data: data,
+              headers: header
+            })
+        } catch (error) {
+            console.error(error)
+            throw error
+        }
+      
     }
 
-    static async getAccessToken(id, secret) {
+    static async getAccessToken() {
 
         const body = {
             grant_type: 'client_credentials',
-            client_id: id,
-            client_secret: secret
+            client_id: config.line.id,
+            client_secret: config.line.secret
         }
 
         const formData = querystring.stringify(body)
@@ -37,7 +62,7 @@ export class Line {
             return token
 
         } catch(error) {
-            console.log(error)
+            console.error(error)
             throw error
         }
     }
